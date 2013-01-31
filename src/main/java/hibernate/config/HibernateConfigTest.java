094148bd5fdee9191ca4f408f2b2782a9f562b5e
Mic <misvy@vmware.com>
2013-01-31 11:28:44 +0800

    removed unneeded @DirtiesContext

--- src/main/java/hibernate/config/HibernateConfigTest.java
+++ src/main/java/hibernate/config/HibernateConfigTest.java
@@ -7,14 +7,12 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="config-spring-hibernate.xml")
-@DirtiesContext
 public class HibernateConfigTest {
 	
 	@Autowired
